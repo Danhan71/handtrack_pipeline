fb559a11fe1c623cd0e17309d4280c2df401dce2
Daniel Hanuska <djhanuska@gmail.com>
2024-10-16 16:32:41 -0400

    server path changes

---
+++ setup/pyvm_all/pyvm/globals.py
@@ -0,0 +1,6 @@
+from pythonlib.globals import MACHINE
+BASEDIR = '/home/dhanuska/dhanuska/hand_track'
+PYVM_DIR = '/home/dhanuska/dhanuska/handtrack_pipeline/setup/pyvm_all/pyvm'
+WINDOWS = False
+NCAMS=4
+CB_DIR='/home/dhanuska/dhanuska/handtrack_pipeline/checkerboard_calib/220914_calib'
