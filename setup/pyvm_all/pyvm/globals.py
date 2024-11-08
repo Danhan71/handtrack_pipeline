cabe1c339c5fabd352eabb8b461a972c8f71468a
Gandalf <djhanuska@gmail.com>
2024-11-08 17:35:41 -0500

    "sheeeesh"

--- setup/pyvm_all/pyvm/globals.py
+++
@@ -1,6 +0,0 @@
-from pythonlib.globals import MACHINE
-BASEDIR = '/home/danhan/Documents/hand_track'
-PYVM_DIR = '/home/danhan/Documents/pipeline/setup/pyvm_all/pyvm'
-WINDOWS = False
-NCAMS=4
-CB_DIR='/home/danhan/Documents/pipeline/checkerboard_calib/220914_calib'
