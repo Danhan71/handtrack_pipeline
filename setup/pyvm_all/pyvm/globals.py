e407669e403a11633bef3f1dc7f8634c41fc4968
Daniel Hanuska <djhanuska@gmail.com>
2024-11-20 16:03:07 -0500

    "dieeeeee"

--- setup/pyvm_all/pyvm/globals.py
+++
@@ -1,6 +0,0 @@
-from pythonlib.globals import MACHINE
-BASEDIR = '/data3/hand_track'
-PYVM_DIR = '/home/danhan/Documents/pipeline/setup/pyvm_all/pyvm'
-WINDOWS = False
-NCAMS=4
-CB_DIR='/home/danhan/Documents/pipeline/checkerboard_calib/220412_calib'
