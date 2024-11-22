b836e84c934d3ca17ea54194a05baed4d1046fea
Gandalf <djhanuska@gmail.com>
2024-11-22 16:32:08 -0500

    "meow"

--- setup/drawmonkey_/drawmonkey/tools/handtrack.py
+++ setup/drawmonkey_/drawmonkey/tools/handtrack.py
@@ -899,7 +899,7 @@ class HandTrack(object):
                         max_ind = i
                         break
             from random import sample
-            # assert max_ind < 5, "Comment this out if you are aware of why the first 5+ frames are bad, this is just a check as most seem to be a few frames"
+            assert max_ind < 5, "Comment this out if you are aware of why the first 5+ frames are bad, this is just a check as most seem to be a few frames"
             volt_align = volt_times[max_ind:]
             # rand_list = sample(range(len(cam_times)), len(volt_align))
             # cam_align = cam_times[rand_list]
