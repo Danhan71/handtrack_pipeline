53fa5ee383db1e0655e2107af6ac92cf7d58bf45
Daniel Hanuska <djhanuska@gmail.com>
2025-04-25 17:13:27 -0400

    'changes to run scripts'

--- pipeline-scripts/diego_run.sh
+++ pipeline-scripts/diego_run.sh
@@ -3,7 +3,7 @@
 #Script to run dates in background on server, with log files
 
 # dates=(230913 231118)
-dates=(230726 230816 230817 230913 230914 231116 231118 240822 240827 230920 230921 230922 230924 230925)
+dates=(230726 230816 230817 230913 230914 231116 231118 240822 240827 230920 230921 230922 230924 230925 230723 230724 230727 230728 230730 230815 230915 250319 250320 250321)
 
 animal="Diego"
 
@@ -17,7 +17,7 @@ do
     else
         dir_type="l"
     fi
-    taskset -a -c 10-19 nohup ../notrain_run_script.sh -e ${i} -b -a ${animal} --loop -${dir_type} > ./logs/${animal}/${i}.log 2>&1 &
+    taskset -a -c 10-19 nohup ../notrain_run_script.sh -e ${i} -b -a ${animal} --loop -${dir_type} --ploton > ./logs/${animal}/${i}.log 2>&1 &
     wait
     echo "$(tail -1000 ./logs/${animal}/${i}.log)" > ./logs/${animal}/${i}.log
 done
--- pipeline-scripts/pancho_run.sh
+++ pipeline-scripts/pancho_run.sh
@@ -4,7 +4,7 @@
 
 # dates=(230616 220716 240508 240524 230622 230623 230626)
 #220604 -- has some trial mismatch in cams, if crips up more then handle otherwise will just ignore
-dates=(231114 231116 220902 220906 220907 220908 220909 230920 230921 230923 231019 231020 240828 240829)
+dates=(231114 231116 220902 220906 220907 220908 220909 230920 230921 230923 231019 231020 240828 240829 220829 220830 220831 220901 230810 230811 230824 230826 230829 250321 250322 250324 250325)
 # dates=(231114)
 animal="Pancho"
 
@@ -18,7 +18,7 @@ do
     else
         dir_type="l"
     fi
-    taskset -a -c 0-9 nohup ../notrain_run_script.sh -e ${i} -b -a ${animal} --loop -${dir_type} > ./logs/${animal}/${i}.log 2>&1 &
+    taskset -a -c 0-9 nohup ../notrain_run_script.sh -e ${i} -b -a ${animal} --loop -${dir_type} --ploton > ./logs/${animal}/${i}.log 2>&1 &
     wait
     echo "$(tail -1000 ./logs/${animal}/${i}.log)" > ./logs/${animal}/${i}.log
 done
